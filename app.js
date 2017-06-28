cef6f07d4095acc13b1427e3b2e15b4bcbc9fecb
Tony Thomas <tonythomas22@protonmail.com>
2017-06-28 16:08:02 -0700

    pug

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var pug = require('pug');
 var port = 4000;
 var app = express();
 
@@ -9,16 +10,23 @@ app.use(function(req, res, next){
     next();
 });
 
+app.set('view engine', 'pug');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.send("Hello World");
+    res.render('index', {
+        title: "Hello World",
+        showTitle: true,
+        people: ['John', 'Chris', 'Frank']
+    });
 });
 
 app.get('/about', function(req, res){
-    res.send("About Page")
+    res.send("About Page");
 });
 
 app.listen(port);
